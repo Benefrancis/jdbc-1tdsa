6673adabad736e81d14cc77d0ae72a68ebd6ea70
Benefrancis do Nascimento <benefrancis@gmail.com>
2023-09-25 17:14:55 -0300

    Melhoria na ConnectionFactory.java

--- src/main/java/br/com/fiap/infra/ConnectionFactory.java
+++ src/main/java/br/com/fiap/infra/ConnectionFactory.java
@@ -7,7 +7,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Objects;
 import java.util.Properties;
